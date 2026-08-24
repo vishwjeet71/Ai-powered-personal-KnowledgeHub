8d5754a9ddda20df10bcda3f178e0470f402386b
vishwjeet <vishwjeetmanwar71@gmail.com>
2026-08-24 11:48:01 +0530

    Connect chat page to backend

--- Personal Knowledge Hub/src/pages/Chat.jsx
+++ Personal Knowledge Hub/src/pages/Chat.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react"
+
 export default function Chat() {
+    const [modelResponse, setModelResponse] = useState("");
+    const [userInput, setUserInput] = useState("");
+
+    const makeRequest = async () => {
+        setModelResponse("Working..")
+        const modelOutput = await fetch("http://localhost:8000/chat",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+
+                body: JSON.stringify({ "query": userInput })
+            }
+        );
+
+        const data = await modelOutput.json(); 
+
+        setModelResponse(data);
+    }
+
     return (
         <>
-        <h2>Hellow from Chat page!</h2>
+            <h2>Hellow from Chat page!</h2>
+            <input value={userInput} placeholder="Enter your query" onChange={(e) => setUserInput(e.target.value)} />
+            <button onClick={makeRequest}>Send</button>
+            {modelResponse}
         </>
     )
 }

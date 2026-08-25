7842f67df917bf7f7f5ac9350e6c85d73276e2cf
vishwjeet <vishwjeetmanwar71@gmail.com>
2026-08-25 18:32:14 +0530

    refactor: move message state to model configuration

--- Personal Knowledge Hub/src/pages/Settings.jsx
+++ Personal Knowledge Hub/src/pages/Settings.jsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
-
 // components
 import ModelConfiguration from "../components/ModelConfiguration";
 
 export default function Settings() {
-    const [displayMessage, setDisplayMessage] = useState("");
-
     return (
         <>
             <div>
-                <ModelConfiguration setDisplayMessage= {setDisplayMessage} />
-                <div>{displayMessage}</div>
+                <ModelConfiguration />
             </div>
         </>
     )
